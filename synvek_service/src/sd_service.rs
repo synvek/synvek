82f239783db1a381718105d41ee685b6d4da8745
SeanYang <sean.yang.ai@outlook.com>
2025-11-23 21:12:20 +0800

    support cache for dll of stable-diffusion.cpp

--- synvek_service/src/sd_service.rs
+++ synvek_service/src/sd_service.rs
@@ -1,7 +1,8 @@
+use crate::common::MODEL_SOURCE_MODELSCOPE;
 use crate::model_service::ModelServiceArgs;
-use crate::{modelscope_helper, utils};
 use crate::{common, fetch_service};
 use crate::{config, file_service};
+use crate::{modelscope_helper, utils};
 use base64::engine::general_purpose::STANDARD;
 use base64::{
     Engine as _, alphabet,
@@ -9,13 +10,13 @@ use base64::{
 };
 use hf_hub::{Cache, Repo, RepoType};
 use libloading::{Library, Symbol};
+use std::collections::HashMap;
 use std::ffi::{CString, OsString, c_char, c_int};
 use std::marker::PhantomData;
 use std::path::PathBuf;
 use std::ptr;
 use std::ptr::null_mut;
 use std::sync::{Arc, Mutex, OnceLock};
-use crate::common::MODEL_SOURCE_MODELSCOPE;
 
 #[repr(C)]
 pub struct ImageOutput {
@@ -52,6 +53,8 @@ pub struct GenerationArgs {
 
 static GLOBAL_SD_CONFIG: OnceLock<Arc<Mutex<SdConfig>>> = OnceLock::new();
 
+type LibraryCache = Arc<Mutex<HashMap<String, Arc<Library>>>>;
+
 fn init_sd_config() -> Arc<Mutex<SdConfig>> {
     Arc::new(Mutex::new(SdConfig::default()))
 }
@@ -80,6 +83,11 @@ pub fn set_sd_config(sd_config: SdConfig) {
     old_sd_config.acceleration = sd_config.acceleration;
 }
 
+fn get_library_cache() -> &'static LibraryCache {
+    static CACHE: OnceLock<LibraryCache> = OnceLock::new();
+    CACHE.get_or_init(|| Arc::new(Mutex::new(HashMap::new())))
+}
+
 fn get_model_file_path(
     model_source: &str,
     repo_name: &str,
@@ -106,7 +114,8 @@ fn get_model_file_path(
     } else {
         model_path.push(format!("models--{}", model_path_postfix));
     }
-    let mut pointer_path = modelscope_helper::get_pointer_path(model_path.to_str().unwrap(), commit_hash);
+    let mut pointer_path =
+        modelscope_helper::get_pointer_path(model_path.to_str().unwrap(), commit_hash);
     pointer_path.push(file_name);
     pointer_path
 }
@@ -199,93 +208,105 @@ pub fn generate_image(generation_args: &GenerationArgs) -> Vec<String> {
     let lib_name = utils::get_backend_path(lib_name.as_str());
 
     tracing::info!("synvek_backend_sd lib_name: {}", lib_name);
+    let library_cache = get_library_cache();
+    let mut library_cache_guard = library_cache.lock().unwrap();
+    let library_arc: Arc<Library>;
+    if let Some(library_arc_ref) = library_cache_guard.get(&lib_name) {
+        library_arc = library_arc_ref.clone();
+    } else {
+        unsafe {
+            let library = libloading::Library::new(lib_name.clone());
+            if let Ok(library) = library {
+                library_arc = Arc::new(library);
+                library_cache_guard.insert(lib_name.clone(), library_arc.clone());
+            } else {
+                return output;
+            }
+        }
+    }
     unsafe {
-        let lib = Library::new(lib_name);
-        if let Ok(lib) = lib {
-            let generate_image_data_func = lib.get(b"generate_image_data");
-            let get_image_count_func = lib.get(b"get_image_count");
-            let get_image_data_length_func = lib.get(b"get_image_data_length");
-            let get_image_data_func = lib.get(b"get_image_data");
-            let free_image_data_func = lib.get(b"free_image_data");
-            match (
-                generate_image_data_func,
-                get_image_count_func,
-                get_image_data_length_func,
-                get_image_data_func,
-                free_image_data_func,
-            ) {
-                (
-                    Ok(generate_image_data_func),
-                    Ok(get_image_count_func),
-                    Ok(get_image_data_length_func),
-                    Ok(get_image_data_func),
-                    Ok(free_image_data_func),
-                ) => {
-                    let generate_image_data: Symbol<GenerateImageData> = generate_image_data_func;
-                    let get_image_count: Symbol<GetImageCount> = get_image_count_func;
-                    let get_image_data_length: Symbol<GetImageDataLength> =
-                        get_image_data_length_func;
-                    let get_image_data: Symbol<GetImageData> = get_image_data_func;
-                    let free_image_data: Symbol<FreeImageData> = free_image_data_func;
-                    let start_args: Vec<String> = vec![
-                        String::from("synvek_service"),
-                        String::from("--diffusion-model"),
-                        model_file_path.to_str().unwrap().to_string(),
-                        String::from("--vae"),
-                        vae_path.to_str().unwrap().to_string(),
-                        String::from("--clip_l"),
-                        clip_l_path.to_str().unwrap().to_string(),
-                        String::from("--t5xxl"),
-                        t5xxl_path.to_str().unwrap().to_string(),
-                        String::from("-p"),
-                        String::from(generation_args.prompt.clone()),
-                        String::from("--cfg-scale"),
-                        String::from("1.0"),
-                        String::from("--sampling-method"),
-                        String::from("euler"),
-                        String::from("-v"),
-                        String::from("--seed"),
-                        String::from(generation_args.seed.to_string()),
-                        String::from("--steps"),
-                        String::from("4"),
-                        String::from("--batch-count"),
-                        String::from(generation_args.n.to_string()),
-                        String::from("--clip-on-cpu"),
-                    ];
-                    let c_start_strings = start_args
-                        .iter()
-                        .map(|s| CString::new(s.as_str()))
-                        .collect::<anyhow::Result<Vec<_>, _>>();
-                    if let Ok(c_start_strings) = c_start_strings {
-                        let raw_ptrs: Vec<*const c_char> =
-                            c_start_strings.iter().map(|cs| cs.as_ptr()).collect();
-                        let image_output =
-                            generate_image_data(start_args.len() as c_int, raw_ptrs.as_ptr());
+        let generate_image_data_func = library_arc.get(b"generate_image_data");
+        let get_image_count_func = library_arc.get(b"get_image_count");
+        let get_image_data_length_func = library_arc.get(b"get_image_data_length");
+        let get_image_data_func = library_arc.get(b"get_image_data");
+        let free_image_data_func = library_arc.get(b"free_image_data");
+        match (
+            generate_image_data_func,
+            get_image_count_func,
+            get_image_data_length_func,
+            get_image_data_func,
+            free_image_data_func,
+        ) {
+            (
+                Ok(generate_image_data_func),
+                Ok(get_image_count_func),
+                Ok(get_image_data_length_func),
+                Ok(get_image_data_func),
+                Ok(free_image_data_func),
+            ) => {
+                let generate_image_data: Symbol<GenerateImageData> = generate_image_data_func;
+                let get_image_count: Symbol<GetImageCount> = get_image_count_func;
+                let get_image_data_length: Symbol<GetImageDataLength> = get_image_data_length_func;
+                let get_image_data: Symbol<GetImageData> = get_image_data_func;
+                let free_image_data: Symbol<FreeImageData> = free_image_data_func;
+                let start_args: Vec<String> = vec![
+                    String::from("synvek_service"),
+                    String::from("--diffusion-model"),
+                    model_file_path.to_str().unwrap().to_string(),
+                    String::from("--vae"),
+                    vae_path.to_str().unwrap().to_string(),
+                    String::from("--clip_l"),
+                    clip_l_path.to_str().unwrap().to_string(),
+                    String::from("--t5xxl"),
+                    t5xxl_path.to_str().unwrap().to_string(),
+                    String::from("-p"),
+                    String::from(generation_args.prompt.clone()),
+                    String::from("--cfg-scale"),
+                    String::from("1.0"),
+                    String::from("--sampling-method"),
+                    String::from("euler"),
+                    String::from("-v"),
+                    String::from("--seed"),
+                    String::from(generation_args.seed.to_string()),
+                    String::from("--steps"),
+                    String::from("4"),
+                    String::from("--batch-count"),
+                    String::from(generation_args.n.to_string()),
+                    String::from("--clip-on-cpu"),
+                ];
+                let c_start_strings = start_args
+                    .iter()
+                    .map(|s| CString::new(s.as_str()))
+                    .collect::<anyhow::Result<Vec<_>, _>>();
+                if let Ok(c_start_strings) = c_start_strings {
+                    let raw_ptrs: Vec<*const c_char> =
+                        c_start_strings.iter().map(|cs| cs.as_ptr()).collect();
+                    let image_output =
+                        generate_image_data(start_args.len() as c_int, raw_ptrs.as_ptr());
 
-                        if image_output == null_mut() {
-                            panic!("Failed to get string array from DLL");
-                        }
-
-                        let image_count = get_image_count(image_output);
-                        tracing::info!("Image count = {}", image_count);
-                        for i in 0..image_count {
-                            let image_data_length = get_image_data_length(image_output, i);
-                            let image_data = get_image_data(image_output, i);
-                            let image_data_slice: &[u8] =
-                                std::slice::from_raw_parts(image_data, image_data_length);
-                            tracing::info!("Image data = {:?}", image_data_slice.len());
-                            let base64_string = STANDARD.encode(image_data_slice);
-                            let data_url = format!("data:image/png;base64,{}", base64_string);
-                            output.push(data_url);
-                        }
-                        tracing::info!("Image generation is finished and release resource now");
-                        free_image_data(image_output);
-                        tracing::info!("Resource release is done.");
+                    if image_output == null_mut() {
+                        panic!("Failed to get string array from DLL");
                     }
+
+                    let image_count = get_image_count(image_output);
+                    tracing::info!("Image count = {}", image_count);
+                    for i in 0..image_count {
+                        let image_data_length = get_image_data_length(image_output, i);
+                        let image_data = get_image_data(image_output, i);
+                        let image_data_slice: &[u8] =
+                            std::slice::from_raw_parts(image_data, image_data_length);
+                        tracing::info!("Image data = {:?}", image_data_slice.len());
+                        let base64_string = STANDARD.encode(image_data_slice);
+                        let data_url = format!("data:image/png;base64,{}", base64_string);
+                        output.push(data_url);
+                    }
+                    tracing::info!("Image generation is finished and release resource now");
+                    free_image_data(image_output);
+                    tracing::info!("Resource release is done.");
                 }
-                _ => {
-                    tracing::error!("Failed to load functions on synvek_backend_sd");
-                }
+            }
+            _ => {
+                tracing::error!("Failed to load functions on synvek_backend_sd");
             }
         }
     }
